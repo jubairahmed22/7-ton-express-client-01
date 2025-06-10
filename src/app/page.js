148cb497aea7025ed33ee5635bd4d30222ecd9a0
jubairahmed22 <jubairahmed060@gmail.com>
2025-06-11 02:16:01 +0600

    something is added

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="text-center py-10 ">
       <h1 className="text-3xl font-bold mb-4 fontPoppins">7tonexpress</h1>
-      {/* <p className="text-lg">A complete authentication system with Next.js and Node.js</p> */}
+      {/* <p className="text-lg">A complete authentication asdf system with Next.js and Node.js</p> */}
       <ChunkData></ChunkData>
     </div>
   )
